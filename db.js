957c4257eb63efb13d85aabd34d4f0e19e1bea14
Denzil Williams <denzzilwilliams@gmail.com>
2020-06-21 10:59:35 -0500

    minor push

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb://localhost:27017/RightNowTaxi",
+  { useNewUrlParser: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoBD Connection Successful");
+    } else {
+      console.log("Error in DB Connection:" + err);
+    }
+  }
+);
 
-mongoose.connect('mongodb://localhost:27017/RightNowTaxi', {useNewUrlParser: true}, (err) => {
-    if (!err) {console.log("MongoBD Connection Successful")}
-    else {console.log('Error in DB Connection:' + err)}
-});
+require("./models/cab.model");
+require("./models/trip.model");
 
-require('./models/cab.model');
-require('./models/trip.model');
-require('./models/user.model');
-
+require("./models/user.model");
